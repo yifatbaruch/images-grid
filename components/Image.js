46fad97975da129a19ff6e1845703cfc3389d8e3
yifatbaruch <85611851+yifatbaruch@users.noreply.github.com>
2021-06-10 09:21:51 +0300

    Add files via upload

--- components/Image.js
+++ components/Image.js
@@ -3,14 +3,12 @@ import './Image.css';
 
 
 function Image(props){
+    
     return(
-
-        <div >
+        <div className={"imageContainer"}>
             <img className = {"image"} src={props.src.url} alt={"blabla"}/>
-            <div className={"textDisplay"}>
-                    <p>{props.src.description}</p>
-                    <p>🖤{props.src.likes}</p>
-            </div>
+            <div className={"description"}>{props.src.description}</div>
+            <p className={"textDisplay"}>🖤   {props.src.likes}</p>
         </div>
     );
 }
